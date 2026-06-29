4cb560e0d5523413f19c5eb691d8e6bf96eb937d
Sampriti-git <sampriti5425@gmail.com>
2026-06-29 19:50:24 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 // Wi-Fi Setup Page
+
 const saveBtn = document.getElementById("saveBtn");
 
 if (saveBtn) {
+
     saveBtn.addEventListener("click", () => {
+
         const ssid = document.getElementById("ssid").value;
         const password = document.getElementById("password").value;
 
+
         if (ssid === "" || password === "") {
+
             alert("Please enter Wi-Fi Name and Password.");
             return;
+
         }
 
-        // This is where we'll send the Wi-Fi details to the ESP32 later.
 
-        window.location.href = "control.html";
+        // Wi-Fi details will be sent to ESP32 later
+
+        window.location.href = "./control.html";
+
     });
+
 }
 
-// Control Buttons
+
+
+// ESP32 Control Section
+
+// Change this later when ESP32 is connected
+const esp32 = "http://YOUR_ESP32_IP";
+
+
+
 const buttons = {
 
     forward: "F",
@@ -27,35 +44,43 @@ const buttons = {
     stop: "S"
 
 };
-};
+
+
 
 Object.keys(buttons).forEach(id => {
+
+
     const btn = document.getElementById(id);
+
 
     if (!btn) return;
 
-    btn.addEventListener("mousedown", () => {
-       const esp32 = "http://YOUR_ESP32_IP";
-    });
 
-    btn.addEventListener("mouseup", () => {
-        console.log("Stop");
-        // We'll send the stop command here when the button is released.
-    });
 
     btn.addEventListener("mousedown", () => {
 
-    let command = buttons[id];
 
-    fetch(esp32 + "/" + command);
+        let command = buttons[id];
 
-});;
+
+        fetch(esp32 + "/" + command)
+        .then(() => console.log(command))
+        .catch(() => console.log("ESP32 not connected"));
+
+
     });
+
+
 
     btn.addEventListener("mouseup", () => {
 
-    fetch(esp32 + "/stop");
 
-});
+        fetch(esp32 + "/S")
+        .catch(() => console.log("ESP32 not connected"));
+
+
     });
+
+
+
 });
